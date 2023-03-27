0f870daecfd5df6150c1c92bc7741c8c99aa3d58
Youssef Ismail <yismail@ualberta.ca>
2023-03-27 12:50:50 -0600

    Add get all games

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,4 @@
-use rocket::{http::Status, serde::json::Json};
+use rocket::{futures::TryStreamExt, http::Status, serde::json::Json};
 use rocket_db_pools::{
     mongodb::{self, Collection},
     Connection, Database,
@@ -17,7 +17,7 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/game/create", data = "<game>")]
+#[post("/create", data = "<game>")]
 async fn create_game(db: Connection<Db>, game: Json<ConnectGame>) -> Result<(), Status> {
     let collection: Collection<ConnectGame> = db.database("mongodb_main").collection("games");
 
@@ -29,9 +29,29 @@ async fn create_game(db: Connection<Db>, game: Json<ConnectGame>) -> Result<(), 
     }
 }
 
+#[get("/all")]
+async fn all_games(db: Connection<Db>) -> Result<Json<Vec<ConnectGame>>, Status> {
+    let collection: Collection<ConnectGame> = db.database("mongodb_main").collection("games");
+
+    let result = collection.find(None, None).await;
+
+    match result {
+        Ok(cursor) => {
+            let games = cursor.try_collect::<Vec<ConnectGame>>().await;
+
+            match games {
+                Ok(games) => Ok(Json(games)),
+                Err(_) => Err(Status::InternalServerError),
+            }
+        }
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::init())
-        .mount("/", routes![create_game, index]) // INDEX MUST BE LAST SINCE ROCKET WILL MATCH THE FIRST ROUTE IT FINDS
+        .mount("/game", routes![all_games, create_game])
+        .mount("/", routes![index]) // INDEX MUST BE LAST SINCE ROCKET WILL MATCH THE FIRST ROUTE IT FINDS
 }
